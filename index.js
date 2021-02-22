2ebcc0a6e25d7de480ea6693866384e41946886a
Dhruvil Shah <dhruvil7797@gmail.com>
2021-02-22 18:38:32 -0500

    Project Setup Completed

---
+++ index.js
@@ -0,0 +1,13 @@
+const express =  require('express') 
+const app = express();
+
+
+
+app.get("/", (req,res) => {
+    res.send("Hello")
+})
+
+
+app.listen(8080, () => {
+    console.log("running")
+})
