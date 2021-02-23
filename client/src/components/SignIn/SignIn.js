f5d1b8c34d70accc1c5fcf482cbac31ba01d2fa3
Dhruvil Shah <dhruvil7797@gmail.com>
2021-02-22 20:37:34 -0500

    UI Completed

---
+++ client/src/components/SignIn/SignIn.js
@@ -0,0 +1,115 @@
+import React, {useState, useEffect} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import  { useHistory} from 'react-router-dom'
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Material Ui
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+
+
+export default function SignIn() {
+  const classes = useStyles();
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  const [user , setUser] = useState({
+    email:"",
+    password:""
+  })
+  const history = useHistory()
+
+
+
+const loginUser = (e) => {
+  e.preventDefault();
+  //TODO:Call API
+  setisLoggedIn(true)
+  history.push("/books")
+
+} 
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form}  noValidate onSubmit={(e) =>{loginUser(e)}}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            onChange={(e) => { setUser({...user, email:e.target.value}) }}
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            onChange={(e) => { setUser({...user, password:e.target.value}) }}
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+
+    </Container>
+  );
+}
